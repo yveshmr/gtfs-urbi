27d1d3b83e9d8bb2a8f7c716f82d2dd3e78777e9
Yves Ribeiro <yveshmr@gmail.com>
2025-12-18 15:56:25 -0300

    config: unify GTFS_STATIC and GTFS_RT URLs

--- app/config.py
+++ app/config.py
@@ -8,3 +8,10 @@ GTFS_DIR = DATA_DIR / "gtfs"
 GTFS_STATIC_URL = "https://servicos.cittati.com.br/GTFS_PLATAFORMA/URBI/GTFS_URBI.zip"
 GTFS_RT_VEHICLE_POSITIONS_URL = "https://servicos.cittati.com.br/GTFS-RT2/URBI/vehicle-positions"
 
+# GTFS-RT – posições dos veículos
+URL_VEHICLE_POSITIONS = "https://servicos.cittati.com.br/GTFS-RT2/URBI/vehicle-positions"
+
+# GTFS estático – já existente, deixamos assim organizado
+URL_GTFS_STATIC_ZIP = "https://servicos.cittati.com.br/GTFS_PLATAFORMA/URBI/GTFS_URBI.zip"
+
+
